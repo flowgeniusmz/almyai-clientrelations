9e382f7bfa14b77615b80d8dbf0b2982ae6bebcf
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-09 15:18:25 -0600

    Create 1_Staggered_Chat.py

---
+++ pages/1_Staggered_Chat.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from openai import OpenAI
+from functions import pagesetup as ps
+
+ps.set_title("AlmyAI", "Client Relations Assistant")
+ps.set_page_overview("Overview", "**Client Relations Assistant** provides a way to quickly ask about the troubleshooting for client relations")
+
+
+
+
+ps.set_blue_header("Operating Manual")
+
+
+ps.set_blue_header("AAO")
+
+
+ps.set_blue_header("Operating Manual")
+
+
+ps.set_blue_header("Process Flow")
+
