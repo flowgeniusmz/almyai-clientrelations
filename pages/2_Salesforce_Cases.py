bc9adf9c8cae3b03e6693c2ab99fb1d29e8e107c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-11 14:39:51 -0600

    Update 2_Salesforce_Cases.py

--- pages/2_Salesforce_Cases.py
+++ pages/2_Salesforce_Cases.py
@@ -13,7 +13,7 @@ def fetch_cases():
         SELECT Id, AccountId, Account.Name, Account.ShippingStreet, Account.ShippingCity,
         Account.ShippingState, Account.ShippingPostalCode, Account.ShippingLongitude,
         Account.ShippingLatitude, Type, Status, Queues__c, Owner.Name, CreatedDate 
-        FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' 
+        FROM Case Where Account.Subsidiary__c = 'Alma Lasers , Inc.' AND Status = 'In Progress'
         ORDER BY CreatedDate DESC 
         LIMIT 200
         """
